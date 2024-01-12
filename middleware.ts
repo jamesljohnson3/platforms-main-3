bd6464e1af90e62bc7efc7388806ed644988701b
Admin <support@unlimitpotential.com>
2024-01-12 00:24:44 -0500

    s

--- middleware.ts
+++ middleware.ts
@@ -52,7 +52,6 @@ export default async function middleware(req: NextRequest) {
   }
 
   
-
   // rewrite everything else to `/[domain]/[slug] dynamic route
   return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
 }
