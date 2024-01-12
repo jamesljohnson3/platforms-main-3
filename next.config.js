bb8c17ddac8383ebc6ba114d7af5f0bf2cfbf766
Admin <support@unlimitpotential.com>
2024-01-12 01:39:24 -0500

    e

--- next.config.js
+++ next.config.js
@@ -1,23 +1,15 @@
-/**
- * @type {import('next').NextConfig}
- */
-module.exports = {
-  experimental: {
-    serverActions: {
-      allowedOrigins: ["app.localhost:3000"],
-    },
-  },
-  images: {
-    remotePatterns: [
-      { hostname: "public.blob.vercel-storage.com" },
-      { hostname: "res.cloudinary.com" },
-      { hostname: "abs.twimg.com" },
-      { hostname: "pbs.twimg.com" },
-      { hostname: "avatar.vercel.sh" },
-      { hostname: "avatars.githubusercontent.com" },
-      { hostname: "www.google.com" },
-      { hostname: "flag.vercel.app" },
-      { hostname: "illustrations.popsy.co" },
-    ]
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'www.orbis.com.ar',
+            port: '',
+            pathname: '/wp-content/themes/barberry/images/**',
+          },
+        ],
+      },
+}
+
+module.exports = nextConfig
